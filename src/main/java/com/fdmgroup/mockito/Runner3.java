9746cb9538d177c743f69d25dce123a5589efd6f
sudheep.kottekudiyil <sudheep.kottekudiyil@fdmgroup.com>
2019-04-05 15:38:32 +0100

    update

---
+++ src/main/java/com/fdmgroup/mockito/Runner3.java
@@ -0,0 +1,21 @@
+package com.fdmgroup.mockito;
+
+public class Runner3 implements Arithmetic {
+
+	public double getX() {
+
+
+		return 0;
+	}
+
+	public String getY() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int multiplyByTwo(int number) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
